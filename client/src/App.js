391bbcf5172e0a3b23d3f8dae8fb900026588a38
Joanna Chądzyńska <jmalek87@gmail.com>
2020-01-06 15:10:44 +0100

    Add booklist component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import BookList from "./components/BookList";
 
 function App() {
-	return <div className='App'>app</div>;
+	return (
+		<div id='main'>
+			<h1>Reading List</h1>
+			<BookList />
+		</div>
+	);
 }
 
 export default App;
